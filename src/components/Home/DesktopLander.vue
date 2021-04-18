c8947a68d191c4417022c8b7c87922933307e7a8
chiagozie Arinzechi <imakandiboi@yahoo.com>
2021-04-18 15:29:45 +0100

    update on carosuel with vue-agile

--- src/components/Home/DesktopLander.vue
+++ src/components/Home/DesktopLander.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <agile>
-      <div class="slide">
-        <img :key="slide" alt="image" />
+    <agile class="main" :autoplay="true" :autoplay-speed="3000" :dots="true">
+      <div
+        class="slide bg-gray-900"
+        v-for="(slide, index) in slides"
+        :key="index"
+      >
+        <img :src="slides" alt="image" width="1900px" />
       </div>
     </agile>
   </div>
@@ -19,12 +23,12 @@ export default {
     return {
       slides: [
         "https://images.unsplash.com/photo-1453831362806-3d5577f014a4?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-        "https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-        "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
+        "https://media.idownloadblog.com/wp-content/uploads/2020/11/MacBook-Pro-M1-processor.jpg",
+        "https://www.pslivegame.com/images/pzen_slideshow/PS5-banner_1614148675.png",
         "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
         "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
         "https://images.unsplash.com/photo-1472926373053-51b220987527?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-        "https://images.unsplash.com/photo-1497534547324-0ebb3f052e88?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
+        "https://www8.hp.com/content/dam/sites/worldwide/homepage/images/banners/us/US-premium-laptops-desktop.jpg",
       ],
     };
   },
@@ -32,11 +36,64 @@ export default {
 </script>
 
 <style scoped>
+.agile__slide img {
+  object-fit: cover;
+}
+
+.agile {
+  width: 100%;
+}
+.agile__actions {
+  margin-top: 20px;
+}
+.agile__nav-button {
+  background: transparent;
+  border: none;
+  color: #ccc;
+  cursor: pointer;
+  font-size: 24px;
+  transition-duration: 0.3s;
+}
+
+.agile__nav-button:hover {
+  color: white;
+}
+
 .slide {
   align-items: center;
   color: #fff;
   display: flex;
-  height: 210px;
+  height: 610px;
   justify-content: center;
+}
+
+.main >>> .agile__nav-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: 0.3s;
+  color: #fff;
+}
+
+.main >>> .agile__nav-button:hover {
+  background-color: rgba(0, 0, 0, 0.9);
+  color: #fff;
+}
+
+.main >>> .agile__nav-button {
+  background-color: rgba(0, 0, 0, 0.4);
+
+  width: 40px;
+  height: 80px;
+}
+
+.main >>> .agile__nav-button--prev {
+  left: 0px;
+  border-radius: 0 8px 8px 0;
+}
+
+.main >>> .agile__nav-button--next {
+  right: 0;
+  border-radius: 8px 0px 0px 8px;
 }
 </style>
